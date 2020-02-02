b3e0a1781af5e8820a9bfd5d681afc77f02d6889
mhenrixon <mikael@mhenrixon.com>
2020-02-02 17:36:11 +0100

    Fix version spec

--- spec/rubocop/mhenrixon_spec.rb
+++ spec/rubocop/mhenrixon_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Rubocop::Mhenrixon do
   it "has a version number" do
-    expect(Rubocop::Mhenrixon::VERSION).to eq "0.79.0"
+    expect(Rubocop::Mhenrixon::VERSION).to eq "0.79.4"
   end
 end
